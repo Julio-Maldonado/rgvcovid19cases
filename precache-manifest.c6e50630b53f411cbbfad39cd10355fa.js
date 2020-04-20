d5cae09b01d1dd6850a2e7c2fcbcd0658b187d08
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-04-20 03:24:21 -0500

    Updates

---
+++ precache-manifest.c6e50630b53f411cbbfad39cd10355fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32c09d41c36e3bcf724c2197fda9907",
+    "url": "/rgvcovid19cases/index.html"
+  },
+  {
+    "revision": "252458be207b1928d13e",
+    "url": "/rgvcovid19cases/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1568be317c910b491980",
+    "url": "/rgvcovid19cases/static/css/main.72591994.chunk.css"
+  },
+  {
+    "revision": "252458be207b1928d13e",
+    "url": "/rgvcovid19cases/static/js/2.32cfe26d.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/rgvcovid19cases/static/js/2.32cfe26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1568be317c910b491980",
+    "url": "/rgvcovid19cases/static/js/main.e9e930df.chunk.js"
+  },
+  {
+    "revision": "a858a6d77ab36a7787e3",
+    "url": "/rgvcovid19cases/static/js/runtime-main.4ed479ca.js"
+  }
+]);
