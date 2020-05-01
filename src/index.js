61174e4fe5f982ca799ec97028b67ff53c922997
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2020-05-01 00:52:46 -0500

    fixed routing 404 problem using HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Routes from './containers/Routes';
 import history from './utils/history';
@@ -13,9 +13,9 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={history}>
+  <HashRouter history={history}>
     <Routes />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
