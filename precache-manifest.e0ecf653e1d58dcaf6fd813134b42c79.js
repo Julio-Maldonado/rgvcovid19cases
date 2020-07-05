44bd204c32f05ca69909b13244b6f9e91a6d4b5b
Julio Maldonado <juliom@qualtrics.com>
2020-07-05 15:29:37 -0700

    Updates

--- precache-manifest.e0ecf653e1d58dcaf6fd813134b42c79.js
+++ precache-manifest.e0ecf653e1d58dcaf6fd813134b42c79.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "02d13d3608a028ac6b712d1494f1e6a5",
+    "revision": "a9f7ef9f9e2a3a662722e96df9ab5d1f",
     "url": "/index.html"
   },
   {
-    "revision": "314893c4443db3589c30",
+    "revision": "b3b792e9813d03c2e9cd",
     "url": "/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "0c3d065abaa4ac91575b",
+    "revision": "b908f07e25454431b090",
     "url": "/static/css/main.3afd0c30.chunk.css"
   },
   {
-    "revision": "314893c4443db3589c30",
-    "url": "/static/js/2.8329403d.chunk.js"
+    "revision": "b3b792e9813d03c2e9cd",
+    "url": "/static/js/2.d1994c44.chunk.js"
   },
   {
     "revision": "d953b79bdca8c0d0c17c1b45ab5c5ca7",
-    "url": "/static/js/2.8329403d.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d1994c44.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0c3d065abaa4ac91575b",
-    "url": "/static/js/main.f21b303c.chunk.js"
+    "revision": "b908f07e25454431b090",
+    "url": "/static/js/main.5d16b9e5.chunk.js"
   },
   {
     "revision": "2206ca89fa9dc69fd9c4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f73c70001b3b4abc9cab0474bbf19d0.js"
+  "/precache-manifest.e0ecf653e1d58dcaf6fd813134b42c79.js"
 );
 
 self.addEventListener('message', (event) => {
